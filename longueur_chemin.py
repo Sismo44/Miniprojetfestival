892de8f7614f2a333c79a71a40bdcb8f90ef8be0
Dobby <bouguerclement4@gmail.com>
2025-09-24 11:55:15 +0200

    j'ai ajouté le commentaire

--- longueur_chemin.py
+++ longueur_chemin.py
@@ -3,6 +3,6 @@ def longueur_chemin(p):
     """
     total = 0
     for i in range (len(p)-1):
-        total = total + distance(scenes,p[i],p[i+1])
+        total = total + distance(scenes,p[i],p[i+1])  #distance entre deux scènes de la liste p
     return total
     
