648f475c479ad2265337bb511b29f7e6876ac4fb
Sismo44 <simonloison2@gmail.com>
2025-09-24 12:24:31 +0200

    Update Codederecherchedunescène.py

--- Codederecherchedunescène.py
+++ Codederecherchedunescène.py
@@ -1,20 +1,25 @@
-from pagebiblio import *
+from pagebiblio import scenes, running_order, souhaits
+
+listesouhait = list(souhaits.items())
+liste_tuple = []
 
 def artiste_scene(artiste):
-    """ Renvoi la scène pour un nom d'artiste, ainsi que ses coordonnées.
+    """ Renvoi une liste de tuple contenant une scène pour un nom d'artiste, ainsi que ses coordonnées et son horaire.
     E : nom de l'artiste (str)
-    S : (tuple) scène, coordonnées, horaires
+    S : une liste de tuple, (tuple) : scène, coordonnées, horaires
     """
-    scene_order = list(scenes.keys())
-    for horaires, artistes in running_order.items():
+    scene_order = list(scenes.keys())    # Création d'une liste qui utilise les clés du dictio scenes situé sur la page biblio
+    for horaires, artistes in running_order.items(): # utilisation du dictio running_order situé sur la page biblio
         for i in range(len(artistes)):
             if artiste == artistes[i]:
                 scene = scene_order[i]
                 coords = scenes[scene]
-                return (scene, coords, horaires)
-                    
-
-    return None
-    
+                (scene, coords, horaires)
         
-print(artiste_scene("Ihsahn"))    # Test pour "Benediction", renvoie le tuple ("dave", (0,0))
+    return liste_tuple
+                    
+for cle, val in souhaits.items():
+    for groupe in val :
+        print(artiste_scene(groupe))
+
+liste_tuple.append(artiste_scene(groupe))
