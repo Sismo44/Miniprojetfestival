ea2514d395bba1babbd1700e4dbe1846c2f0596c
Louisegrman <louisegerman13@gmail.com>
2025-09-24 11:18:15 +0200

    chemin encore

--- gloutonetexhaustif.py
+++ gloutonetexhaustif.py
@@ -1,4 +1,7 @@
 from math import sqrt
+from random import randint
+from time import time, perf_counter
+from matplotlib import pyplot as plt
 
 scenes = {"dave": (0, 0),
           "suppo": (50, 50),
@@ -47,3 +50,35 @@ def exhaustif(scene):
         return chemin2, round(d2, 1)
 
 print(exhaustif(scenes))
+
+nbre_elements_total = 500
+ensemble_listes = [ [randint(1, 1000) for _ in range(n)] for n in range(2, nbre_elements_total)]
+for liste in ensemble_listes:
+    liste.sort(reverse=True)
+
+# Calculs des durées
+def calculs_durees(fonction, scene):
+    """ Mesure des durées d'exécution de la fonction
+    E:  fonction (object) fonction à exécuter
+        scene (list) liste des scenes
+    S:  mesures_temps (list)
+    """
+    mesures_temps = []
+    for i in scenes:
+        start = perf_counter()
+        exhaustif(scene)     # exécution de la focntion
+        end = perf_counter()
+        mesures_temps.append(end-start)
+    return mesures_temps
+
+
+duree_exhaustif = calculs_durees(exhaustif(scenes), scenes) 
+duree_glouton = calculs_durees(glouton(scenes), scenes)     # ordonnées
+nbre_elements = [n for n in range(0, len(scenes))]  # abscisses
+
+# Tracé
+plt.plot(nbre_elements, duree_exhaustif, "+", color="blue", label="algorithme exhaustif")
+plt.plot(nbre_elements, duree_glouton, ".", color="red", label="algorithme glouton")
+plt.legend()
+plt.grid()
+plt.show()
